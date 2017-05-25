561d9f77a9c339609f6ab09fa0e7f539dbef825e
huanhuan <huanfuan@163.com>
2017-05-25 15:14:32 +0800

    1.0.0

---
+++ 科技数码论坛/src/main/java/com/huan/HuansaoApplication.java
@@ -0,0 +1,12 @@
+package com.huan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuansaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuansaoApplication.class, args);
+	}
+}
